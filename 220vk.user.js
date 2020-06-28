a49ca3a36c35dfd27ae815d670f88bfd6ed9274d
artur98983 <56383065+artur98983@users.noreply.github.com>
2020-06-28 16:37:42 +0300

    Add files via upload

---
+++ 220vk.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         220vk
+// @include      *://220vk.com/*
+// ==/UserScript==
+
+if (document.getElementById("tolstoycommentswrapper7954495249366835")) {
+			document.getElementById("concon")
+            //console.log(document.getElementsByClassName("div.css-1dbjc4n.r-14lw9ot.r-1p0dtai.r-1d2f490.r-1xcajam.r-zchlnj.r-ipm5af"))
+				.setAttribute("style", "display: none;");
+		}
